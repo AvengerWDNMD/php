3b7d33118dfc506c258b343142313389a4b6f7a5
AvengerWDNMD <59591254+AvengerWDNMD@users.noreply.github.com>
2020-12-06 10:27:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,96 @@
 <!DOCTYPE html>
-<html>
-<body>
+<html lang="en-US">
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="/assets/css/style.css?v=851e344bbce24815694e3ad377c19d5aa448e1b8" media="screen" type="text/css">
+    <link rel="stylesheet" href="/assets/css/print.css" media="print" type="text/css">
 
-<h1> 11班网站暂时关闭</h1 >
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 
+<!-- Begin Jekyll SEO tag v2.6.1 -->
+<title>欢迎访问该网站，本网站现已经暂时关闭 | AvengerWDNMD.github.io</title>
+<meta name="generator" content="Jekyll v3.9.0" />
+<meta property="og:title" content="欢迎访问该网站，本网站现已经暂时关闭" />
+<meta property="og:locale" content="en_US" />
+<link rel="canonical" href="http://www.our11.tk/" />
+<meta property="og:url" content="http://www.our11.tk/" />
+<meta property="og:site_name" content="AvengerWDNMD.github.io" />
+<script type="application/ld+json">
+{"@type":"WebSite","headline":"欢迎访问该网站，本网站现已经暂时关闭","url":"http://www.our11.tk/","name":"AvengerWDNMD.github.io","@context":"https://schema.org"}</script>
+<!-- End Jekyll SEO tag -->
+
+  </head>
+
+  <body>
+    <header>
+      <div class="inner">
+        <a href="http://www.our11.tk/">
+          <h1>高21届11班</h1>
+        </a>
+        <h2></h2>
+        
+        
+          <a href="our11.team" class="button"><small>新的域名</small> </a>
+        
+      </div>
+    </header>
+
+    <div id="content-wrapper">
+      <div class="inner clearfix">
+        <section id="main-content">
+          <h1 id="欢迎访问该网站本网站现已经暂时关闭">欢迎访问该网站，本网站现已经暂时关闭</h1>
+<h2 id="由于服务器费用和维护的时间成本以及网站升级网站暂时关闭">由于服务器费用，和维护的时间成本以及网站升级，网站暂时关闭。</h2>
+
+<h2 id="我已经保存了所有的数据并做好了备份">我已经保存了所有的数据，并做好了备份</h2>
+<h3 id="我已将原有的域名升级为our11team">我已将原有的域名升级为our11.team</h3>
+<p>our11.tk将在2021年9月停止访问</p>
+<h1 id="班级网盘将在2020年12月06日关闭">班级网盘将在2020年12月06日关闭</h1>
+<p>如有上传照片请在此之前上传。之后可将照片发给我的QQ</p>
+
+<p>待到全新网站上线时我将一并上传至全新网站。</p>
+
+<p>届时网站的相应速度和实用性都会大幅提升。</p>
+
+<h2 id="你可以在21年7月之前看到全新的11班网站">你可以在21年7月之前看到全新的11班网站</h2>
+
+<h1 id="祝你成功我们山顶见">祝你成功，我们山顶见！</h1>
+<p>—网管</p>
+<p style="text-align:center;">
 <?php
-echo “ 11班网站即将12月6日全部关闭”，
-echo  “将在2021年7月重新开放。” ;
-$ time1 = time（）;
 
-$ time2 = strtotime（ “ 2021-6-7”）;
+$time1 = time();
 
-$ time3 = strtotime（ “ 2021-6-7”）;
+$time2 = strtotime("2021-06-7");
 
-$子=小区（（ $ TIME2 - $时间1）/ 3600）;
+$time3 = strtotime("2021-06-7");
 
-$ SUB2 =小区（（ $时间3 - $时间1）/ 86400）;
+$sub = ceil(($time2 - $time1)/3600);
+
+$sub2 = ceil(($time3 - $time1)/86400);
 
  
 
+echo "祝你成功$sub2"."天后我们山顶见";
 
 
-echo  “距离高考还有$ sub2”。“天！”。'<br>' ;
 ?>
 
-</body>
+        </section>
+
+        <aside id="sidebar">
+          
+
+          
+
+          
+        </aside>
+      </div>
+    </div>
+
+    
+  </body>
 </html>
